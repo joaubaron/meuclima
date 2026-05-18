515a59156c708a7d779f06da4b2de3c820392731
joaubaron <joaubaron@users.noreply.github.com>
2026-05-18 16:49:19 +0000

    deploy: 3dbef8d9a51756336825761f60eddf0ff53a959e

--- components/script.js
+++ components/script.js
@@ -1662,7 +1662,7 @@ cratersData.forEach(([cx,cy,r]) => {
   cr2.setAttribute('clip-path','url(#mclit)');
   svg.appendChild(cr2);
 });
-const closeBtn = document.createElement('button');closeBtn.textContent='Fechar';closeBtn.style.cssText='margin-top:28px;background:transparent;border:none;outline:none;color:#ffeb3b;padding:8px 28px;border-radius:20px;font-size:14px;cursor:pointer;';closeBtn.addEventListener('focus',()=>closeBtn.style.outline='none');closeBtn.addEventListener('mousedown',e=>e.preventDefault());closeBtn.onclick=()=>document.body.removeChild(modal);
+const closeBtn = document.createElement('button');closeBtn.textContent='Fechar';closeBtn.style.cssText='margin-top:28px;background:transparent;border:none;outline:none;color:#ffeb3b;padding:8px 28px;border-radius:20px;font-size:12px;cursor:pointer;';closeBtn.addEventListener('focus',()=>closeBtn.style.outline='none');closeBtn.addEventListener('mousedown',e=>e.preventDefault());closeBtn.onclick=()=>document.body.removeChild(modal);
 modal.appendChild(titulo);modal.appendChild(svg);modal.appendChild(sub);modal.appendChild(closeBtn);document.body.appendChild(modal);
 };
 
