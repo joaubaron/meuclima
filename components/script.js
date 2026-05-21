a799044d6993c801e05c4ca70bbc8e2ec9b6ac48
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-05-21 08:35:51 -0300

    Update script.js

--- components/script.js
+++ components/script.js
@@ -1802,7 +1802,7 @@ const porStr    = converterHora12para24(astroSol?.sunset);
 sunDivImediato.innerHTML = `
 <div class="info-inline moon-text" style="font-size: 0.95em; overflow-x: auto;">
 <div class="info-item" style="display: flex; align-items: center; flex-wrap: nowrap; gap: 15px; white-space: nowrap;">
-<span>☀️ ${nascerStr} &nbsp;   &nbsp; 🌙 ${porStr}</span>
+<span>☀️ ${nascerStr} &nbsp;     &nbsp; 🌙 ${porStr}</span>
 </div>
 </div>
 `;
